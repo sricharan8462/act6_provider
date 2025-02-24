8d6b92a7cae83d368a27dbb8509701a192eaf586
sricharan8462 <sricharan8462@yahoo.com>
2025-02-24 18:48:40 -0500

    Add Window Size Configuration and Basic UI Enhancements

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  setupWindow();
   runApp(
     ChangeNotifierProvider(
       create: (context) => Counter(),
       child: const MyApp(),
     ),
   );
+}
+
+const double windowWidth = 360;
+const double windowHeight = 640;
+
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    setWindowTitle('Provider Counter App');
+    setWindowMinSize(const Size(windowWidth, windowHeight));
+    setWindowMaxSize(const Size(windowWidth, windowHeight));
+    getCurrentScreen().then((screen) {
+      setWindowFrame(
+        Rect.fromCenter(
+          center: screen!.frame.center,
+          width: windowWidth,
+          height: windowHeight,
+        ),
+      );
+    });
+  }
 }
 
 class Counter with ChangeNotifier {
@@ -33,8 +58,6 @@ class MyApp extends StatelessWidget {
       title: 'Provider Counter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        // Uncomment the following line if you want to use Material 3:
-        // useMaterial3: true,
       ),
       home: const MyHomePage(),
     );
@@ -53,7 +76,6 @@ class MyHomePage extends StatelessWidget {
         child: Consumer<Counter>(
           builder: (context, counter, child) => Text(
             'Counter: ${counter.value}',
-            // If you're using Material 3, use headlineMedium instead of headline4:
             style: Theme.of(context).textTheme.headlineMedium,
           ),
         ),
