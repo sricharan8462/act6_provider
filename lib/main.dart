de5e9ca958f05d0265d5ba24f8301b79c74194d4
sricharan8462 <sricharan8462@yahoo.com>
2025-02-24 18:43:16 -0500

    Integrate Provider for State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Counter(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class Counter with ChangeNotifier {
+  int value = 0;
+  void increment() {
+    value++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    if (value > 0) {
+      value--;
+      notifyListeners();
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -9,63 +30,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Basic Flutter App',
+      title: 'Provider Counter App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        // Uncomment the following line if you want to use Material 3:
+        // useMaterial3: true,
       ),
       home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  void _decrementCounter() {
-    setState(() {
-      if (_counter > 0) {
-        _counter--;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Basic Flutter App'),
+        title: const Text('Provider Counter App'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Counter: $_counter'),
-          ],
+        child: Consumer<Counter>(
+          builder: (context, counter, child) => Text(
+            'Counter: ${counter.value}',
+            // If you're using Material 3, use headlineMedium instead of headline4:
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
         ),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: _decrementCounter,
+            onPressed: () => context.read<Counter>().decrement(),
             tooltip: 'Decrement',
             child: const Icon(Icons.remove),
           ),
           const SizedBox(width: 10),
           FloatingActionButton(
-            onPressed: _incrementCounter,
+            onPressed: () => context.read<Counter>().increment(),
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
